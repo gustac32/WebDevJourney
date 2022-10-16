4724bc35f372621bd7d5dd660ebf4bad644dc0f0
gustac32 <gustavo.matamoros@ue-germany.de>
2022-10-16 12:23:10 +0200

    Add existing file

---
+++ Desktop/Repository/Bored.py
@@ -0,0 +1,19 @@
+# imports
+import random
+import time
+# game vars
+
+snake = ["🐍", 100]
+money = ["💶", 50]
+skull = ["💀", 3]
+lemon = ["🍋", 1]
+cherry = ["🍒", 20]
+eball = ["🎱", 8]
+seven = ["🧿", 7]
+digits = [snake, money, skull, lemon, cherry, eball, seven]
+cash = 50
+
+print("Welcome to the slot machine!")
+print("You have 50$, each spin costs 1$")
+print("Good luck!")
+print()
