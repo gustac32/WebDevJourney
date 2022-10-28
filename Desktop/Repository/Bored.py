dbace1c1be8899b1d2449126d43b6a2da1db1bcb
gustac32 <gustavo.matamoros@ue-germany.de>
2022-10-28 18:26:47 +0200

    /Users/tavs/Desktop/Repository

--- Desktop/Repository/Bored.py
+++ Desktop/Repository/Bored.py
@@ -2,7 +2,6 @@
 import random
 import time
 # game vars
-
 snake = ["🐍", 100]
 money = ["💶", 50]
 skull = ["💀", 3]
@@ -12,8 +11,22 @@ eball = ["🎱", 8]
 seven = ["🧿", 7]
 digits = [snake, money, skull, lemon, cherry, eball, seven]
 cash = 50
-
 print("Welcome to the slot machine!")
 print("You have 50$, each spin costs 1$")
 print("Good luck!")
 print()
+
+# functions
+
+
+def odds(digits):
+    choices = []
+    for _ in range(random.randrange(3, 7)):
+        choice = random.choices(digits)
+        if choice not in choices:
+            choices.append(choice)
+    return choices
+
+
+def spinner(digits):
+    temp =
